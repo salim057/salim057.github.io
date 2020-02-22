a3e5a8b6e2e3bfe2c171c606d42327e11612b583
Salim <salim057@live.com>
2020-02-23 00:20:47 +0530

    Updates

---
+++ precache-manifest.8f4f353c49f62f83bb3c06375b50f7b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19d9b4c138321f79621d0c53a8091e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba2d5707b9f0403c21bf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "53e951adf82f8cd9aca2",
+    "url": "/static/js/2.e837b0ba.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e837b0ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba2d5707b9f0403c21bf",
+    "url": "/static/js/main.e8b9e4a8.chunk.js"
+  },
+  {
+    "revision": "a81ea66f0385c74b70b8",
+    "url": "/static/js/runtime-main.105fd310.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
